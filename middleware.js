8ac99624068cc8a86c72b8f77a506eefa22651bc
PatientTrac <wayne@wayluz.com>
2026-03-09 13:31:20 -0500

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,15 +1,11 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([
-  "/dashboard(.*)",
-  "/report(.*)",
-  "/reports(.*)",
-]);
-
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
-});
+export default clerkMiddleware();
 
 export const config = {
-  matcher: ["/((?!_next|.*\\..*).*)", "/"],
+  matcher: [
+    "/dashboard(.*)",
+    "/report(.*)",
+    "/api(.*)",
+  ],
 };
