ead28abad092b9be2b5c7124bc89844ac58ba7bc
PatientTrac <wayne@wayluz.com>
2026-03-10 11:27:18 -0500

    Create page.tsx

---
+++ app/screener/page.tsx
@@ -0,0 +1,7 @@
+import { ScreenerBuilder } from "@/components/screener/screener-builder";
+
+export const dynamic = "force-dynamic";
+
+export default function ScreenerPage() {
+  return <ScreenerBuilder />;
+}
